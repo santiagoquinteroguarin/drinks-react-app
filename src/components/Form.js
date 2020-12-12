0f669a873353aa72f304333731342c02e64bb06d
Santiago Quintero <quinterosantiago15@gmail.com>
2020-12-12 09:09:42 -0500

    Building form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,41 @@ import React from 'react';
 
 const Form = () => {
     return (
-        
+        <form
+            className="col-12"
+        >
+            <fieldset className="text-center">
+                <legend>Busca bebidas por Categoria o Ingredientes</legend>
+            </fieldset>
+
+            <div className="row mt-4">
+                <div className="col-md-4">
+                    <input 
+                        type="text"
+                        name="name"
+                        className="form-control"
+                        placeholder="Buscar por Ingredientes"
+                    />
+                </div>
+
+                <div className="col-md-4">
+                    <select 
+                        className="form-control"
+                        name="category"
+                    >
+                        <option value="">-- Selecciona Categoria --</option>
+                    </select>
+                </div>
+
+                <div className="col-md-4">
+                    <input 
+                        type="submit"
+                        className="btn btn-block btn-primary"
+                        value="Buscar Bebidas"
+                    />
+                </div>
+            </div>
+        </form>
     );
 }
 
