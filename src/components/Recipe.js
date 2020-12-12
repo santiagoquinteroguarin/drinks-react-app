95b6bd97c810a64b53396d9f26f31db883b6b937
Santiago Quintero <quinterosantiago15@gmail.com>
2020-12-12 11:40:38 -0500

    import Modal component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,45 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import ContextModal from '../context/ContextModal';
 
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+
+function getModalStyle() {
+    const top = 50 ;
+    const left = 50;
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        position: 'absolute',
+        width: 600,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
+
 const Recipe = ({recipe}) => {
+
+    // config modal del material ui
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+
+    const classes = useStyles();
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
 
     const { setSavedIdRecipe } = useContext(ContextModal);
 
@@ -17,11 +55,22 @@ const Recipe = ({recipe}) => {
                         type="button"
                         className="btn btn-block btn-primary"
                         onClick={() => {
-                            setSavedIdRecipe(recipe.idDrink)
+                            setSavedIdRecipe(recipe.idDrink);
+                            handleOpen();
                         }}
                     >
                         Ver Receta
                     </button>
+
+                    <Modal
+                        opem={open}
+                        onClose={() => {
+                            setSavedIdRecipe(null);
+                            handleClose();
+                        }}
+                    >
+                        <div style={modalStyle} className={classes.paper}></div>
+                    </Modal>
                 </div>
             </div>
         </div>
